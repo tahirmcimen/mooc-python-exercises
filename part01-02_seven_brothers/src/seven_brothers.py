d1d115b97bec6f96b8d23e5d036d0c3eb1973561
tahirmcimen <67092534+tahirmcimen@users.noreply.github.com>
2025-07-31 17:15:10 +0300

    second excercise solution

--- part01-02_seven_brothers/src/seven_brothers.py
+++ part01-02_seven_brothers/src/seven_brothers.py
@@ -1,8 +1,9 @@
 # Fix the code
-print("Simeoni")
-print("Juhani")
-print("Eero")
-print("Lauri")
 print("Aapo")
-print("Tuomas")
+print("Eero")
+print("Juhani")
+print("Lauri")
+print("Simeoni")
 print("Timo")
+print("Tuomas")
+
