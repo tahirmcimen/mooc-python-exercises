5ab73fd7c3e5334ec3472de8b199bdbfff84bfb8
tahirmcimen <67092534+tahirmcimen@users.noreply.github.com>
2025-08-01 18:08:48 +0300

    8th exercise solution added

--- part01-08_name_and_address/src/name_and_address.py
+++ part01-08_name_and_address/src/name_and_address.py
@@ -1,1 +1,21 @@
 # Write your solution here
+"""
+Please write a program which asks for the user's name and address. The program should also print out the given information, as follows:
+Sample output
+
+Given name: Steve
+Family name: Sanders
+Street address: 91 Station Road
+City and postal code: London EC05 6AW
+Steve Sanders
+91 Station Road
+London EC05 6AW
+
+"""
+name = input("What is your name?")
+familyname = input("What is your family name?")
+address = input("What is your address?")
+citycode = input("What is your city code?")
+print(name + " " + familyname)
+print(address)
+print(citycode)
