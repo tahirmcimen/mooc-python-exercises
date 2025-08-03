03dce95fbf9f43322d3d021f4d581a073518de44
tahirmcimen <67092534+tahirmcimen@users.noreply.github.com>
2025-08-03 15:03:35 +0300

    13th exercise solution is added

--- part01-13_times_five/src/times_five.py
+++ part01-13_times_five/src/times_five.py
@@ -1,1 +1,12 @@
 # Write your solution here
+"""
+Please write a program which asks the user for a number. The program then prints out the number multiplied by five.
+
+The program should function as follows:
+Sample output
+
+Please type in a number: 3
+3 times 5 is 15
+"""
+number = int(input("Please write a number:"))
+print(f"{number} times 5 is {number*5}")
