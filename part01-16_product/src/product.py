d0ad5d9fea95cf7cdc7272509fdd48d1f8244471
tahirmcimen <67092534+tahirmcimen@users.noreply.github.com>
2025-08-03 15:17:20 +0300

    16th exercise solution is added

--- part01-16_product/src/product.py
+++ part01-16_product/src/product.py
@@ -1,8 +1,22 @@
 # Fix the code
-number = int(input("Please type in the first number: "))
-number = int(input("Please type in the second number: "))
-number = int(input("Please type in the third number: "))
+"""
+This program asks the user for three numbers. The program then prints out their product, that is, the numbers multiplied by each other. There is, however, something wrong with the program - it doesn't work quite right, as you can see if you run it. Please fix it.
 
-product = number * number * number
+An example of the expected execution of the program:
+Sample output
+
+Please type in the first number: 2
+Please type in the second number: 3
+Please type in the third number: 5
+The product is 30
+"""
+number = int(input("Please type in the first number: "))
+product = 1
+product *= number
+number = int(input("Please type in the second number: "))
+product *= number
+number = int(input("Please type in the third number: "))
+product *= number
+
 
 print("The product is", product)
